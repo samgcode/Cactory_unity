f96a0b30366c7ff29f78ca26ed8b153744cdfe94
sam gaudet <samgcode@gmail.com>
2020-08-06 14:21:08 -0600

    - added scene manager to create tiles - added gamemanager to hover over entities

---
+++ Cactory/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        detectHover();
+    }
+
+    Ray ray;
+    RaycastHit2D hit;
+    Hoverable prevEntity;
+    void detectHover() {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        onHover(ray, hit); 
+    } 
+
+    void onHover(Ray ray, RaycastHit2D hit) {
+        hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+        if(hit.collider != null) {
+            if(hit.collider.tag == "hoverable") {
+                Hoverable entity = hit.collider.GetComponent<Hoverable>();
+                entity.hovering = true;
+                if(prevEntity && entity.id != prevEntity.id) {
+                    prevEntity.hovering = false;
+                }
+                prevEntity = entity;
+            }
+        }
+    }
+}
---
+++ Cactory/Assets/Scripts/Managers/SceneManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject tilePrefab;
+    public GameObject cactusTilePrefab;
+    public GameObject ironTilePrefab;
+    void Start()
+    {
+        for(int col = 0; col < 18; col++) {
+            for(int row = 0; row < 11; row++) {
+                GameObject tile;
+                if(Random.Range(0, 5) == 1) {
+                    if(Random.Range(0, 8) == 1) {
+                        tile = Instantiate(ironTilePrefab, new Vector3(col - 8, row - 5, 0), Quaternion.identity);    
+                    } else {
+                        tile = Instantiate(cactusTilePrefab, new Vector3(col - 8, row - 5, 0), Quaternion.identity);
+                    }
+                } else {
+                    tile = Instantiate(tilePrefab, new Vector3(col - 8, row - 5, 0), Quaternion.identity);
+                }
+                Hoverable[] entities = FindObjectsOfType<Hoverable>();
+                tile.GetComponent<Hoverable>().id = entities.Length;
+            }
+        }
+    }
+}
