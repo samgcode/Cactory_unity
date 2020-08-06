af9c7f6fa1b065250fa7dd2ec5d18b66a8f6d33d
sam gaudet <samgcode@gmail.com>
2020-08-06 14:05:26 -0600

    added sprites and animations -added hoverable script

---
+++ Cactory/Assets/Scripts/Hoverable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hoverable : MonoBehaviour
+{
+    public int id = 0;
+    public bool hovering = false;
+    public SpriteRenderer hoveringRenderer;
+    
+    private void Start() {
+        this.tag = "hoverable";
+    }
+
+    private void Update() {
+        if(hovering) {
+            hoveringRenderer.enabled = true;
+        } else {
+            hoveringRenderer.enabled = false;
+        }    
+    }
+}
