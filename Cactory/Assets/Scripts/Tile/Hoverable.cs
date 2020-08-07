f38009318938fb105a4506041f2f5dd0aa1ce506
sam gaudet <samgcode@gmail.com>
2020-08-07 14:55:16 -0600

    - moved hoverable script into new folder - added tile script

---
+++ Cactory/Assets/Scripts/Tile/Hoverable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hoverable : MonoBehaviour
+{
+    public int id = 0;
+    public bool hovering = false;
+    public SpriteRenderer hoveringRenderer;
+    
+    private void Awake() {
+        this.tag = "hoverable";
+        Hoverable[] entities = FindObjectsOfType<Hoverable>();
+        id = entities.Length;
+    }
+
+    private void Update() {
+        if(hovering) {
+            hoveringRenderer.enabled = true;
+        } else {
+            hoveringRenderer.enabled = false;
+        }    
+    }
+}
---
+++ Cactory/Assets/Scripts/Tile/Tile.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public bool hasCactus = false;
+    public bool hasIron = false;
+    public bool hasMachine = false;
+}
